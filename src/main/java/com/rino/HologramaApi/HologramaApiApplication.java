9616357e728088d199405e2593a20bb9a6867ab3
Rino Arias <rinoariaslaaz@gmail.com>
2023-03-16 16:44:57 -0500

    Initial commit

---
+++ src/main/java/com/rino/HologramaApi/HologramaApiApplication.java
@@ -0,0 +1,13 @@
+package com.rino.HologramaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HologramaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HologramaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rino/HologramaApi/HologramaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rino.HologramaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HologramaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
